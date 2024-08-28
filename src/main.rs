1ebcfd08e953611a5a5d01999b2864d4189eb48c
SeraMadeleine <sel063@uit.no>
2024-08-28 12:52:21 +0200

    added so we get addres and port

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 #[macro_use]
-
-
-
-
 extern crate rocket;
 
-#[get("/helloworld")]
-fn hello() -> String {
-    format!("Hello!")
+use rocket::{Build, Rocket, State};
+
+struct ServerConfig {
+    address: String, 
+    port: u16,
 }
 
+#[get("/helloworld")]
+fn hello(config: &State<ServerConfig>) -> String {
+    format!(
+        "Hello, world! The server is running on address: {}, and port: {}", 
+        config.address,
+        config.port
+    )
+}
 
+#[launch]
+fn rocket() -> Rocket<Build> {
+    let config = rocket::Config::figment().extract::<rocket::Config>().unwrap();
+    let server_config = ServerConfig { 
+        address: config.address.to_string(), 
+        port: config.port };
 
-
-
-
-#
-[launch]
-fn rocket() -> _ {
-
-   rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .manage(server_config)
+        .mount("/", routes![hello])
 }
