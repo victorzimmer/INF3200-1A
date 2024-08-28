e30b1faf5fee27f9c34c07539363b038d666f811
SeraMadeleine <sel063@uit.no>
2024-08-28 15:24:51 +0200

    added first draft of bash

---
+++ src/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+regex_positiv_integer="^[0-9]+$"  # regex for positive integer
+
+# See if an argument is provided
+if [ -z "$1" ]; then  # -z checks if the variable is empty
+    echo "No argument provided, please provide the number of servers to start: $0 <number_of_servers>"
+    exit 1
+fi 
+
+# See if the argument is an integer 
+if ! [[ "$1" =~ $regex_positiv_integer ]]; then
+    echo "The argument provided is not an integer, please provide an integer value: $0 <number_of_servers>"
+    exit 1
+fi
+
+# Get available nodes from cluster
+availe_nodes=$(/share/ifi/available-nodes.sh)  # Note the syntax correction here
+
+node_count=$(echo "$availe_nodes" | wc -w)
+# Check if there are enough nodes
+if [ "$1" -gt "${#availe_nodes[@]}" ]; then
+    echo "Not enough nodes available, please provide a smaller number of servers"
+    exit 1
+fi
+
+# Loop through the number of servers to start and assign nodes to servers
+node_list=$(echo "$availe_nodes" | cut -d' ' -f1-"$1")      # converterer til et array:  cut -d' ' -f1-"$1"
+for node in $node_list; do  
+    echo "Starting server on node: $node"  # DEBUG PRINT 
+    port=$(shuf -i 49152-65535 -n 1)       # Get a random port number between 49152 and 65535 <- from the assignment 
+    # Command to start the server with ssh 
+    ## sjekke at podman er installert, last ned conteiner 
+
+    
+done
+
+# Format the output as a JSON list
+json_output=$(printf "\"%s:%s\"," "${availe_nodes[@]:0:$1}" "$port" | sed 's/,$//')
+echo "$json_output"
