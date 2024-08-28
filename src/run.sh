a5cf5f161ab1d979c81597700014274ac4d62558
SeraMadeleine <sel063@uit.no>
2024-08-28 16:44:29 +0200

    tester med en while loop

--- src/run.sh
+++ src/run.sh
@@ -32,14 +32,45 @@ json_output=()
 # TODO: håndtere at det er etterspurt flere servere enn det er noder
 # TODO: sette inn vår main og container
 
-for node in $node_list; do  
-    echo "Starting server on node: $node"  # DEBUG PRINT 
-    port=$(shuf -i 49152-65535 -n 1)       # Get a random port number between 49152 and 65535 <- from the assignment 
-    # Command to start the server with ssh 
-    ## sjekke at podman er installert, last ned conteiner 
-    ssh -f $node "echo $HOST; exit"
-    json_output+=("\"$node:$port\"")
+# for node in $node_list; do      
+#     echo "Starting server on node: $node"  # DEBUG PRINT 
+#     port=$(shuf -i 49152-65535 -n 1)       # Get a random port number between 49152 and 65535 <- from the assignment 
+#     # Command to start the server with ssh 
+#     ## sjekke at podman er installert, last ned conteiner 
+#     ssh -f $node "echo $HOST; exit"
+#     json_output+=("\"$node:$port\"")
+# done
+
+
+requested_nodes = $1 
+
+while (( requested_nodes > 0 )); do 
+    if (( requested_nodes > node_count )); then 
+        for node in "${available_nodes[@]}"; do
+            echo "Starting server on node: $node"  # DEBUG PRINT 
+            port=$(shuf -i 49152-65535 -n 1)       # Get a random port number between 49152 and 65535 
+            # Command to start the server with ssh 
+            ssh -f $node "echo $HOST; exit"
+            json_output+=("\"$node:$port\"")
+        done
+        requested_nodes=$((requested_nodes - node_count))
+    else
+        for ((i=0; i<requested_nodes; i++)); do
+            node="${available_nodes[$i]}"
+            echo "Starting server on node: $node"  # DEBUG PRINT 
+            port=$(shuf -i 49152-65535 -n 1)       # Get a random port number between 49152 and 65535 
+            # Command to start the server with ssh 
+            ssh -f $node "echo $HOST; exit"
+            json_output+=("\"$node:$port\"")
+        done
+        requested_nodes=0
+    fi
 done
+
+# Format the output as a JSON list
+json_result="[$(IFS=,; echo "${json_output[*]}")]"
+echo "$json_result"
+
 
 # Format the output as a JSON list
 json_result="[$(IFS=,; echo "${json_output[*]}")]"
