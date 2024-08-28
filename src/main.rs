68e5f3d0567283ed8f626f7125103b07970a4848
SeraMadeleine <sel063@uit.no>
2024-08-28 13:02:53 +0200

    proøver noe

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,36 @@ extern crate rocket;
 use rocket::{Build, Rocket, State};
 
 struct ServerConfig {
-    address: String, 
-    port: 50090,
+    address: String,
+    port: u16,
 }
 
 #[get("/helloworld")]
 fn hello(config: &State<ServerConfig>) -> String {
     format!(
-        "Hello, world! The server is running on address: {}, and port: {}", 
+        "Hello, world! The server is running on address: {}, and port: {}",
         config.address,
-        config.port
+        config.port,
     )
 }
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    let config = rocket::Config::figment().extract::<rocket::Config>().unwrap();
-    let server_config = ServerConfig { 
-        address: config.address.to_string(), 
-        port: config.port };
+    let address = "127.0.0.1".to_string(); // Du kan endre til en annen adresse hvis ønskelig
+    let port = 50900; // Definer porten her
 
-    rocket::build()
+    let config = rocket::Config {
+        address: address.clone(),
+        port,
+        ..rocket::Config::default()
+    };
+
+    let server_config = ServerConfig {
+        address,
+        port,
+    };
+
+    rocket::custom(config)
         .manage(server_config)
         .mount("/", routes![hello])
 }
