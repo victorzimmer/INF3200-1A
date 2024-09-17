128bdb87d37b680b158f36d8851aa7e49f27e9c0
SeraMadeleine <sel063@uit.no>
2024-09-17 13:52:02 +0200

    Psudo code for endpoints

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,35 @@ extern crate rocket;
 
 use rocket::State;
 use std::env;
-use sha2::{Digest, Sha256};
+use std::sync::{Arc, Mutex};
+// use sha2::{Digest, Sha256};
+
 
 struct Node {
     id: String,
     address: String,
     successor: Option<String>,
-    predecessor: Option<String>,
+    precessor: Option<String>,
     finger_table: Vec<String>,
-    data: Arc<Mutex<HashMap<String, String>>,       // datastruktur som inneholder en HashMap hvor både nøklene og verdiene er String
+    data: Arc<Mutex<HashMap<String, String>>      // datastruktur som inneholder en HashMap hvor både nøklene og verdiene er String
 }
 
 struct A1Config {
     hostname: String,
     port: String,
-    node: Arc<Mutex<Node>>,         // Mutex to ensure that only one thread can modify it at a time, Arc to allow multiple threads to have access to this protected Node
+    node: Node    // Mutex to ensure that only one thread can modify it at a time, Arc to allow multiple threads to have access to this protected Node
 }
 
-
-
 // function that takes in a key (as a string) and returns a int (u64)
-fn hash(key: &str) -> u64 {
+fn hash(key: &str) -> _ {
     let mut hasher = Sha256::new();          // create a new Sha256 - 256 byte (32-bytes) hash
     hasher.update(key);                      // update the hasher with the key
     let result = hasher.finalize();          // finalize the hasher and store the result in the variable result
-    
 }
 
 #[get("/helloworld")]
 fn helloworld(a1_config: &State<A1Config>) -> String {
-    format!("{}:{}", a1_config.hostname, a1_config.port)
+    format!("{}:{}", a1_config.hostname, a1_config.port);
 }
 
 #[get("/storage/<key>")]
@@ -51,23 +50,27 @@ fn get_network()-> _ {
 }
 
 #[get("/ring/precessor")]
-fn get_precessor()-> _ {
+fn get_precessor(a1_config: &State<A1Config>) -> String {
     println!("Get precessor");
+    format!("{}", a1_config.node.precessor);
 }
 
 #[get("/ring/successor")]
-fn get_successor()-> _ {
+fn get_successor(a1_config: &State<A1Config>) -> String {
     println!("Get successor");
+    format!("{}", a1_config.node.successor);
 }
 
 #[put("/ring/precessor")]
-fn put_precessor()-> _ {
-    println!("Get precessor");
+fn put_precessor(a1_config: &State<A1Config>, newPrecessor: &str) -> String {
+    println!("Put precessor");
+    a1_config.node.precessor = newPrecessor;
 }
 
 #[put("/ring/successor")]
-fn put_successor()-> _ {
-    println!("Get successor");
+fn put_successor(a1_config: &State<A1Config>, newSuccessor: &str) -> String {
+    println!("Put successor");
+    a1_config.node.successor = newSuccessor;
 }
 
 #[get("/ring/finger_table")]
@@ -83,14 +86,26 @@ fn calculate_finger_table()-> _ {
 
 #[launch]
 fn rocket() -> _ {
+    let node = Node {
+        id: env::var("ID").expect("id not provided"),
+        address: env::var("address").expect("address not provided"),
+        successor: _,
+        precessor: _,
+        finger_table: vec!(),
+        data: vec!(),
+    };
+
     let a1_config = A1Config {
         hostname: env::var("A1_HOSTNAME").expect("Hostname not provided!"),
         port: env::var("A1_PORT").expect("Port not provided!"),
+        node: node,
     };
+
+
 
     rocket::build()
         .manage(a1_config)
-        .mount("/", routes![helloworld, get_storage, put_storage, get_network, 
-                            get_precessor, get_successor, put_precessor, 
+        .mount("/", routes![helloworld, get_storage, put_storage, get_network,
+                            get_precessor, get_successor, put_precessor,
                             put_successor, get_finger_table, calculate_finger_table])
 }
