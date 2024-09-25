344fbc8c3c19bc3902b098b1173a2384763e9515
Victor Zimmer <victor.zimmer@me.com>
2024-09-25 23:44:32 +0200

    Added dynamic joining, and longest range calculation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::response::status::{BadRequest, NoContent};
+use rocket::http::Status;
+use rocket::response::status::{self, BadRequest, Conflict, Created, Custom, NoContent};
 use rocket::serde::Deserialize;
 use rocket::serde::{json::Json, Serialize};
 use rocket::State;
@@ -12,13 +13,7 @@ use std::sync::{Arc, RwLock};
 mod storage;
 use storage::Storage;
 
-const CHORD_RING_SIZE: u32 = 255 ^ 20; // Currently set to maximum sum of hash function output (20 bytes)
-
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(crate = "rocket::serde")]
-struct SuppliedNetworkInformation {
-    network_id: String,
-}
+const RING_SIZE: u16 = u16::MAX; // Maximum size of the ring, and thereby maximum number of nodes supported
 
 // Node represent a node in the cluster
 #[derive(Serialize, Deserialize, Clone)]
@@ -26,18 +21,59 @@ struct SuppliedNetworkInformation {
 struct Node {
     hostname: String,
     port: u16,
-    position: u32,
-    range: u32,
+    position: u16,
+    range: u16,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct Network {
+    network_id: String,
 }
 
 struct NodeConfig {
-    network_id: Option<String>,
+    network: Option<Network>,
     connected: bool,
     local: Node,
     successor: Option<Node>,
     precessor: Option<Node>,
     finger_table: Vec<Node>,
     storage: Storage,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct JoinNetworkInformation {
+    network: Network,
+    position: u16,
+    range: u16,
+    successor: Node,
+    precessor: Node,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct SuppliedNetworkInformation {
+    network_id: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct SuppliedNode {
+    hostname: String,
+    port: u16,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct LongestRangeRequest {
+    started_by: Node,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct LongestRangeResponse {
+    longest_range: Node,
 }
 
 // // function that takes in a key (as a string) and returns a int (u64)
@@ -174,16 +210,257 @@ fn get_network(node_config: &State<Arc<RwLock<NodeConfig>>>) -> Json<Vec<String>
 fn put_network_initialize(
     node_config: &State<Arc<RwLock<NodeConfig>>>,
     network_information: Json<SuppliedNetworkInformation>,
-) -> () {
+) -> Result<String, Conflict<&str>> {
+    let mut config = node_config.write().expect("RWLock is poisoned");
+
+    if config.connected {
+        return Err(status::Conflict("Node is already connected to network"));
+    }
+
+    config.connected = true;
+    config.network = Some(Network {
+        network_id: network_information.network_id.clone(),
+    });
+
+    config.local.position = 0;
+    config.local.range = RING_SIZE;
+    config.precessor = Some(config.local.clone());
+    config.successor = Some(config.local.clone());
+
+    return Ok(format!(
+        "Initialized network with network_id: {}",
+        config
+            .network
+            .clone()
+            .map_or(String::from("default"), |network| network.network_id)
+    ));
+}
+
+#[get("/network/longest_range")]
+fn get_network_longest_range(
+    node_config: &State<Arc<RwLock<NodeConfig>>>,
+) -> Result<Json<LongestRangeResponse>, Custom<String>> {
+    let config = node_config.read().expect("RWLock is poisoned");
+    let longest_range_request = LongestRangeRequest {
+        started_by: config.local.clone(),
+    };
+
+    if !config.connected {
+        let error_message = String::from(
+            "Node is not in a network and therefore can't provide information on longest range.",
+        );
+        println!("{}", &error_message);
+        return Err(status::Custom(Status::FailedDependency, error_message));
+    }
+
+    let successor = config.successor.as_ref().expect("No successor");
+
+    let longest_range_uri = format!(
+        "http://{}:{}/network/longest_range",
+        successor.hostname, successor.port
+    );
+    let upstream_response = match minreq::post(longest_range_uri)
+        .with_json(&longest_range_request)
+        .expect("Unable to serialize request")
+        .send()
+    {
+        Err(_err) => {
+            let error_message = format!(
+                "Could not connect to successor node: {}:{}.",
+                successor.hostname, successor.port
+            );
+            println!("{}", &error_message);
+            return Err(status::Custom(Status::FailedDependency, error_message));
+        }
+        Ok(upstream_response) => upstream_response,
+    };
+
+    if upstream_response.status_code != 200 {
+        let error_message = format!(
+            "Successor node did not provide longest range. Node responded: [{} - {}] {}",
+            upstream_response.status_code,
+            upstream_response.reason_phrase,
+            upstream_response.as_str().unwrap_or("Unparseable content")
+        );
+        println!("{}", &error_message);
+        return Err(status::Custom(Status::FailedDependency, error_message));
+    }
+
+    let longest_range_upstream = match upstream_response.json::<LongestRangeResponse>() {
+        Err(_err) => {
+            let error_message =
+                String::from("Unable to parse received longest range information from JSON.");
+            println!("{}", &error_message);
+            return Err(status::Custom(Status::FailedDependency, error_message));
+        }
+        Ok(longest_range_upstream) => longest_range_upstream,
+    };
+
+    return Ok(Json(longest_range_upstream));
+}
+
+#[post("/network/longest_range", data = "<longest_range_request>")]
+fn post_network_longest_range(
+    node_config: &State<Arc<RwLock<NodeConfig>>>,
+    longest_range_request: Json<LongestRangeRequest>,
+) -> Result<Json<LongestRangeResponse>, Custom<String>> {
+    let config = node_config.read().expect("RWLock is poisoned");
+
+    if !config.connected {
+        let error_message = String::from(
+            "Node is not in a network and therefore can't provide information on longest range.",
+        );
+        println!("{}", &error_message);
+        return Err(status::Custom(Status::FailedDependency, error_message));
+    }
+
+    if longest_range_request.0.started_by.hostname == config.local.hostname
+        && longest_range_request.0.started_by.port == config.local.port
+    {
+        let longest_range_response = LongestRangeResponse {
+            longest_range: config.local.clone(),
+        };
+        return Ok(Json(longest_range_response));
+    } else {
+        let successor = config.successor.as_ref().expect("No successor");
+
+        let longest_range_uri = format!(
+            "http://{}:{}/network/longest_range",
+            successor.hostname, successor.port
+        );
+        let upstream_response = match minreq::post(longest_range_uri)
+            .with_json(&longest_range_request.0)
+            .expect("Unable to serialize request")
+            .send()
+        {
+            Err(_err) => {
+                let error_message = String::from("Could not connect to successor node.");
+                println!("{}", &error_message);
+                return Err(status::Custom(Status::FailedDependency, error_message));
+            }
+            Ok(upstream_response) => upstream_response,
+        };
+
+        if upstream_response.status_code != 200 {
+            let error_message = format!(
+                "Successor node did not provide longest range. Node responded: [{} - {}] {}",
+                upstream_response.status_code,
+                upstream_response.reason_phrase,
+                upstream_response.as_str().unwrap_or("Unparseable content")
+            );
+            println!("{}", &error_message);
+            return Err(status::Custom(Status::FailedDependency, error_message));
+        }
+
+        let longest_range_upstream = match upstream_response.json::<LongestRangeResponse>() {
+            Err(_err) => {
+                let error_message =
+                    String::from("Unable to parse received longest range information from JSON.");
+                println!("{}", &error_message);
+                return Err(status::Custom(Status::FailedDependency, error_message));
+            }
+            Ok(longest_range_upstream) => longest_range_upstream,
+        };
+
+        if longest_range_upstream.longest_range.range >= config.local.range {
+            return Ok(Json(longest_range_upstream));
+        } else {
+            let longest_range_response = LongestRangeResponse {
+                longest_range: config.local.clone(),
+            };
+            return Ok(Json(longest_range_response));
+        }
+    }
+}
+
+#[get("/network/request_join_network_information")]
+fn get_network_request_join(
+    node_config: &State<Arc<RwLock<NodeConfig>>>,
+) -> Result<Json<JoinNetworkInformation>, Custom<&str>> {
+    if !node_config.read().expect("RWLock is poisoned").connected {
+        println!("Node is not in a network and therefore can't provide information to join.");
+        return Err(status::Custom(
+            Status::FailedDependency,
+            "Node is not in a network and therefore can't provide information to join.",
+        ));
+    }
+
+    // let join_network_information = JoinNetworkInformation {
+    //     network: node_config
+    //         .read()
+    //         .expect("RWLock is poisoned")
+    //         .network
+    //         .expect("Node was connected, but had no network")
+    //         .clone(),
+    //     position: 0,
+    //     range: 0,
+    //     successor: None,
+    //     precessor: None,
+    // };
+
     todo!();
 }
 
 #[put("/network/join", data = "<existing_node>")]
 fn put_network_join(
     node_config: &State<Arc<RwLock<NodeConfig>>>,
-    existing_node: Json<String>,
-) -> () {
-    todo!();
+    existing_node: Json<SuppliedNode>,
+) -> Result<String, Custom<String>> {
+    let join_uri = format!(
+        "http://{}:{}/network/request_join_network_information",
+        existing_node.0.hostname, existing_node.0.port
+    );
+
+    let join_response = match minreq::get(join_uri).send() {
+        Err(_err) => {
+            let error_message = String::from("Could not connect to existing node.");
+            println!("{}", &error_message);
+            return Err(status::Custom(Status::FailedDependency, error_message));
+        }
+        Ok(join_reponse) => join_reponse,
+    };
+
+    if join_response.status_code != 200 {
+        let error_message = format!(
+            "Existing node denied join request. Node responded: [{} - {}] {}",
+            join_response.status_code,
+            join_response.reason_phrase,
+            join_response.as_str().unwrap_or("Unparseable content")
+        );
+        println!("{}", &error_message);
+        return Err(status::Custom(Status::FailedDependency, error_message));
+    }
+
+    let received_network_information = match join_response.json::<JoinNetworkInformation>() {
+        Err(_err) => {
+            let error_message =
+                String::from("Unable to parse received network information from JSON.");
+            println!("{}", &error_message);
+            return Err(status::Custom(Status::FailedDependency, error_message));
+        }
+        Ok(received_network_information) => received_network_information,
+    };
+
+    let mut config = node_config.write().expect("RWLock is poisoned");
+
+    config.connected = true;
+    config.network = Some(Network {
+        network_id: received_network_information.network.network_id,
+    });
+    config.local.position = received_network_information.position;
+    config.local.range = received_network_information.range;
+    config.successor = Some(received_network_information.successor);
+    config.precessor = Some(received_network_information.precessor);
+
+    return Ok(format!(
+        "Joined network with ID: {}",
+        config
+            .network
+            .clone()
+            .map_or(String::from("Unknown"), |network| network
+                .network_id
+                .clone())
+    ));
 }
 
 #[launch]
@@ -202,7 +479,7 @@ fn rocket() -> _ {
         precessor: None,
         finger_table: vec![],
         storage: Storage::new(),
-        network_id: None,
+        network: None,
         connected: false,
     }));
 
@@ -235,6 +512,9 @@ fn rocket() -> _ {
             put_successor,
             get_finger_table,
             calculate_finger_table,
+            get_network_request_join,
+            get_network_longest_range,
+            post_network_longest_range,
             put_network_initialize,
             put_network_join
         ],
