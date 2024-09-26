4c73d4249537a3df2ef0e63c4cbb9259dd8ade6b
Victor Zimmer <victor.zimmer@me.com>
2024-09-26 15:44:36 +0200

    Updated binary to download

--- src/run-node.sh
+++ src/run-node.sh
@@ -3,7 +3,7 @@
 echo "Deploying on node $nodename:$port"
 
 BINARY_FILE="inf2300-a1-bin-x86_64-unknown-linux-gnu"
-BINARY_FILE_URL="https://github.com/victorzimmer/INF3200-1A/releases/download/v0.1.0/inf2300-a1-bin-x86_64-unknown-linux-gnu"
+BINARY_FILE_URL="https://github.com/SeraMadeleine/INF3200-1B/releases/download/v0.2.0/inf2300-a1-bin-x86_64-unknown-linux-gnu"
 
 if [ -f $BINARY_FILE ]; then
    echo "Binary already present."
